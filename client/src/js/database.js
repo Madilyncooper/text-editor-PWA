d45c7bbba1ba3662b649d790594d5d046398baa0
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-07-17 19:19:39 -0700

    Fixed indexDb bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,28 +12,29 @@ const initdb = async () =>
     },
   });
 
-export const putDb = async (content) => {
-
-  const database = await openDB('jate', 1);
-  const readADD = database.transaction('jate', 'readwrite');
-  const savedContent = readADD.objectStore('jate');
-  const newDB = savedContent.put({ id: 1, value: content });
-  const updatedDb = await newDB;
-  console.log('Data saved to the database', updatedDb.value);
-
-};
-
-export const getDb = async () => {
-
-  const database = await openDB('jate', 1);
-  const content = database.transaction('jate', 'readonly');
-  const savedContent = content.objectStore('jate');
-  const readDb = savedContent.get(1);
-  const result = await readDb;
+  export const putDb = async (content) => {
+    console.log('PUT to the database');
+    const database = await openDB('jate', 1);
+    const updateDb = database.transaction('jate', 'readwrite');
+    const saveDb = updateDb.objectStore('jate');
+    const request = saveDb.put({ id: 1, value: content });
+    const result = await request;
+    console.log('Data saved to the database', result.value);
+  };
   
-  if (!result) {console.log('Data not found in the database');}
-  console.log('Data was READ', result.value)
-  return result?.value;
-};
+
+  export const getDb = async () => {
+    console.log('GET from the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    result
+      ? console.log('This is data from Database', result.value)
+      : console.log('Unable to find data in database');
+    return result?.value;
+  };
+  
 
 initdb();
